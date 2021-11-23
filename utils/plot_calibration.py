ed805de27366ca59512b85ee6ef82725ca27d5e6
Sam McSweeney <sammy.mcsweeney@gmail.com>
2021-11-23 16:58:08 +0800

    Cleaned up plot_calibration.py a bit

--- utils/plot_calibration.py
+++ utils/plot_calibration.py
@@ -15,6 +15,62 @@ def get_tilenames(filename, first_line=14, nlines=16):
 
     return tilenames
 
+def plot_tile(ax, PP, PQ, QP, QQ, phases_or_amps='phases', show_yticks=False, **kwargs):
+    ax.scatter(np.arange(len(PP)), PQ, c='cyan', **kwargs)
+    ax.scatter(np.arange(len(PP)), QP, c='magenta', **kwargs)
+    ax.scatter(np.arange(len(PP)), PP, c='b', **kwargs)
+    ax.scatter(np.arange(len(PP)), QQ, c='r', **kwargs)
+
+    ax.set_xticklabels([])
+    if phases_or_amps == 'phases':
+        ax.set_ylim([-np.pi, np.pi])
+        ax.set_yticks([-np.pi/2, 0, np.pi/2])
+        if show_yticks:
+            ax.set_yticklabels(["-π/2", "0", "π/2"])
+        else:
+            ax.set_yticklabels([])
+    elif phases_or_amps == 'amps':
+        ax.set_yticks([0, 1, 2])
+        if show_yticks:
+            ax.set_yticklabels(["0", "1", "2"])
+        else:
+            ax.set_yticklabels([])
+
+def plot_all_tiles(cal_data, phases_or_amps='phases', tilenames=None, ncols=16, **kwargs):
+
+    print("Plotting {}...".format(phases_or_amps))
+
+    if phases_or_amps == 'phases':
+        column_offset = 1
+    elif phases_or_amps == 'amps':
+        column_offset = 0
+
+    nants = cal_data.shape[1]//8
+    nrows = nants // ncols
+
+    fig, axs = plt.subplots(nrows, ncols, sharex=True, sharey=True);
+
+    for r in range(nrows):
+        for c in range(ncols):
+            ant = r*ncols + c
+
+            show_yticks = (c == 0)
+
+            PP = cal_data[:,ant*8 + 0 + column_offset]
+            PQ = cal_data[:,ant*8 + 2 + column_offset]
+            QP = cal_data[:,ant*8 + 4 + column_offset]
+            QQ = cal_data[:,ant*8 + 6 + column_offset]
+
+            plot_tile(axs[r,c], PP, PQ, QP, QQ, phases_or_amps=phases_or_amps, show_yticks=show_yticks, **kwargs)
+
+            axs[r,c].set_title(tilenames[ant], y=1.0, pad=-14)
+
+    plt.subplots_adjust(wspace=0, hspace=0)
+    fig.suptitle("{}\n[b c]\n[m r]".format(phases_or_amps))
+
+    return fig, axs
+
+
 if __name__ == "__main__":
 
     if len(sys.argv) < 2:
@@ -23,66 +79,12 @@ if __name__ == "__main__":
 
     cal_data = np.loadtxt(sys.argv[-1])
 
-    nchans = cal_data.shape[0]
-    nants = cal_data.shape[1]//8
-
-    ncols = 16
-    nrows = nants // ncols
-
     # Get tilenames
     tilenames = get_tilenames(sys.argv[-1])
 
-    # Phases
-    print("Plotting phases...")
-
-    fig_phase, axs_phase = plt.subplots(nrows, ncols, sharex=True, sharey=True)
-
     s = 0.1
 
-    for r in range(nrows):
-        for c in range(ncols):
-            ant = r*ncols + c
-
-            axs_phase[r,c].scatter(np.arange(nchans), cal_data[:,ant*8 + 3], c='cyan', s=s, marker='.') # PQ phase
-            axs_phase[r,c].scatter(np.arange(nchans), cal_data[:,ant*8 + 5], c='magenta', s=s, marker='.') # QP phase
-            axs_phase[r,c].scatter(np.arange(nchans), cal_data[:,ant*8 + 1], c='b', s=s, marker='.') # PP phase
-            axs_phase[r,c].scatter(np.arange(nchans), cal_data[:,ant*8 + 7], c='r', s=s, marker='.') # QQ phase
-
-            axs_phase[r,c].set_xticklabels([])
-            axs_phase[r,c].set_yticklabels([])
-            axs_phase[r,c].set_ylim([-np.pi, np.pi])
-
-            axs_phase[r,c].set_title(tilenames[ant], y=1.0, pad=-14)
-
-    plt.subplots_adjust(wspace=0, hspace=0)
-    fig_phase.suptitle("Phases\n[b c]\n[m r]")
-
-    '''
-    # Amps
-    print("Plotting amps...")
-
-    fig_amps, axs_amps = plt.subplots(nrows, ncols, sharex=True, sharey=True)
-
-    for r in range(nrows):
-        for c in range(ncols):
-            ant = r*ncols + c
-
-            axs_amps[r,c].scatter(np.arange(nchans), cal_data[:,ant*8 + 2], c='cyan', s=s, marker='.') # PQ amps
-            axs_amps[r,c].scatter(np.arange(nchans), cal_data[:,ant*8 + 4], c='magenta', s=s, marker='.') # QP amps
-            axs_amps[r,c].scatter(np.arange(nchans), cal_data[:,ant*8 + 0], c='b', s=s, marker='.') # PP amps
-            axs_amps[r,c].scatter(np.arange(nchans), cal_data[:,ant*8 + 6], c='r', s=s, marker='.') # QQ amps
-
-            axs_amps[r,c].set_xticklabels([])
-            axs_amps[r,c].set_yticks([0, 1, 2])
-            if c == 0:
-                axs_amps[r,c].set_yticklabels(["0", "1", "2"])
-            else:
-                axs_amps[r,c].set_yticklabels([])
-
-            axs_amps[r,c].set_title(tilenames[ant], y=1.0, pad=-14)
-
-    plt.subplots_adjust(wspace=0, hspace=0)
-    fig_amps.suptitle("Amps\n[b c]\n[m r]")
-    '''
+    plot_all_tiles(cal_data, phases_or_amps='phases', tilenames=tilenames, s=s, marker='.')
+    plot_all_tiles(cal_data, phases_or_amps='amps', tilenames=tilenames, s=s, marker='.')
 
     plt.show()
