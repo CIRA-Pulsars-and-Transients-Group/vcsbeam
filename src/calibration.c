a492ae0925997dc0b1232e2b76b1bb2700957490
Chia Min Tan <chiamintan@Chias-MacBook-Air.local>
2024-02-01 09:52:40 +0800

    fix calibration solution reading

--- src/calibration.c
+++ src/calibration.c
@@ -426,7 +426,7 @@ void read_bandpass_file(
 void vmLoadOffringaSolution( vcsbeam_context *vm )
 {
     // Shorthand variables
-    int coarse_chan_idx = vm->coarse_chan_idxs_to_process[0];
+    int coarse_chan_idx = vm->cal_coarse_chan_idxs_to_process[0];
 
     // Open the calibration file for reading
     FILE *fp = NULL;
