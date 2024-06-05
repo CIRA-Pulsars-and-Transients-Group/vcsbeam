3c18969c8b3c3fc575eae5c2b2eb6d8d6dc5d055
Marcin Sokolowski <marcin.sokolowski@curtin.edu.au>
2024-06-05 17:24:36 +0800

    added MPI module

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ module load cray-hdf5-parallel/1.12.2.7
 module load cfitsio/4.3.0
 module load rust/1.78.0
 module load pal/0.9.8-yyskiux
+module load cray-mpixlate
 
 export LD_LIBRARY_PATH=/scratch/mwavcs/msok/install/mwa_hyperbeam/target/release:/scratch/mwavcs/msok/install/mwalib/target/release:$LD_LIBRARY_PATH
 
